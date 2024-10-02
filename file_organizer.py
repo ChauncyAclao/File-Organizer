8c49db00ece350b3e72df59fcadcb0ba44356aa1
ChauncyAclao <rheignchauncy@gmail.com>
2024-10-02 15:25:54 +0800

    last commit

--- file_organizer.py
+++ file_organizer.py
@@ -1,6 +1,5 @@
 import os
 import collections
-from pprint import pprint
 
 download_path = os.path.join(
     os.path.expanduser("~"),
@@ -12,8 +11,6 @@ for filename in os.listdir(download_path):
     file_type = filename.split(".")[-1]
     file_mappings.setdefault(file_type, []).append(filename)
 
-#pprint(file_mappings)
-
 for folder_name, folder_items in file_mappings.items():
     folder_path = os.path.join(download_path, folder_name)
     if not os.path.exists(folder_path):
@@ -22,4 +19,5 @@ for folder_name, folder_items in file_mappings.items():
     for folder_item in folder_items:
         source = os.path.join(download_path, folder_item)
         destination = os.path.join(folder_path, folder_item)
-        print(f'Moving {source} to {destination}')
+        os.rename(source, destination)
+        
