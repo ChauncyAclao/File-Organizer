ae99ba82d28798004332bcefa0daec405c5f0d31
ChauncyAclao <rheignchauncy@gmail.com>
2024-10-02 15:06:26 +0800

    third commit

--- file_organizer.py
+++ file_organizer.py
@@ -12,5 +12,14 @@ for filename in os.listdir(download_path):
     file_type = filename.split(".")[-1]
     file_mappings.setdefault(file_type, []).append(filename)
 
-pprint(file_mappings)
+#pprint(file_mappings)
 
+for folder_name, folder_items in file_mappings.items():
+    folder_path = os.path.join(download_path, folder_name)
+    if not os.path.exists(folder_path):
+        os.mkdir(folder_path)
+
+    for folder_item in folder_items:
+        source = os.path.join(download_path, folder_item)
+        destination = os.path.join(folder_path, folder_item)
+        print(f'Moving {source} to {destination}')
